6d9d1c9e48dbdb2ea6aa70224122526d5ab04b8b
sidapraj <siddhesh.apraj@algorisys.com>
2020-03-20 13:08:15 +0530

    create a factory folder in spec for word and added factory gem rails

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -4,13 +4,14 @@ RSpec.describe WordsController, type: :controller do
   describe 'GET INDEX' do
     context 'when some words are present' do
       it 'assigns @words' do
-       word = Word.create(value: 'cat', language: 'English')
+      # word = Word.create(value: 'cat', language: 'English')
+      word = create(:word)
       get :index
       expect(assigns(:words)).to eq([word])
     end
   end
 
-  context 'when some words are present' do
+  context 'when words are not present' do
     it 'assigns @words' do
      get :index
      expect(assigns(:words)).to eq([])
