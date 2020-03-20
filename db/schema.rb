bf6321de5fd6ca9277528821529e6df6c59ea507
sidapraj <siddhesh.apraj@algorisys.com>
2020-03-20 08:33:22 +0530

    Added a route

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20200319061414) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "words", force: :cascade do |t|
     t.string   "value"
     t.string   "language"
