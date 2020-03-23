67c914c50ff4d74bca8686dc69a89244fdb5cdb1
sidapraj <siddhesh.apraj@algorisys.com>
2020-03-23 17:34:00 +0530

    Made changes in authorization spec

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -167,11 +167,10 @@ describe 'GET edit' do
 let(:params) do
   { id: word.id }
 end
-let!(:word) { create(:word) }
+let!(:word) { create(:word, user: user) }
+let(:user) { create(:user) }
 
   context 'when user is signed in' do
-    let(:user) { create(:user) }
-
    before do
      sign_in(user)
      subject
@@ -202,13 +201,13 @@ end
 describe 'PUT Update' do
   subject { put :update, params }
 
-  let!(:word) { create(:word, content:'cat', language: language_1) }
+  let!(:word) { create(:word, user: user, content:'cat', language: language_1) }
   let!(:language_1) { create(:language, name: 'English') }
   let!(:language_2) { create(:language, name: 'Polish') }
+  let(:user) { create(:user) }
+
 
   context 'when user is signed in' do
-    let(:user) { create(:user) }
-
    before do
      sign_in(user)
    end
@@ -267,11 +266,11 @@ end
 describe 'Delete destroy' do
   subject { delete :destroy, params }
 
-  let!(:word) { create(:word) }
+  let!(:word) { create(:word, user: user) }
+  let(:user) { create(:user) }
+
 
   context 'when user is signed in' do
-    let(:user) { create(:user) }
-
    before do
      sign_in(user)
    end
